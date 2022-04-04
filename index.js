a6687392e7c026916ef1f7952f7cc72ce673c1ea
Gabriel <gabriel.braz@hotmail.com>
2022-04-04 12:27:58 -0300

    Express

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 
 
 console.log("oi gente");
+
+const express = require('express');
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+res.send('Oi Gente!')
+})
+
+app.get('/usuarios/:id', (req, res) => {
+    let id = req.params.id;
+    res.send('Lista de Usuários...'+id)
+    })
+
+app.listen(port, () => {
+    console.log(`Servidor escutando na porta ${port}`)
+})
